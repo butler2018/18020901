a3564f0ab864294b46c3e190abd537e41d6a11ac
calvin <butler2018@hotmail.com>
2018-02-09 10:29:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/a18020901/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.student.a18020901;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private AdView mAdView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAdView = (AdView) findViewById(R.id.adview);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+
+    }
+}
